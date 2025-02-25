92f3bfcaf57b1f88c744a48663062de208661605
shivhub09 <shivnagori2020@gmail.com>
2025-02-25 13:29:54 +0530

    check changes

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
-import User  from "../models/user.model.js"; 
+import User from "../models/user.model.js";
 
 
 // Controller: Create New User
